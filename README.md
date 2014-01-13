fce94b0828330bf4aa94e688d43322105893cece
Keerthik <keerthiko@gmail.com>
2014-01-12 22:53:34 -0800

    Edited tools

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-armory
+Zoako Armory
 ======
 
-license information
+This is a repository of Zoako LLC game studios Unity editor developer tools. As we work on our games, we add general-purpose functions here that improve the productivity and workflow inside the Unity Editor.
+
+Currently contains several shortcut keys for commonly used Editor functionalities.
+
+License Information
 ===================
 GPL v3. See http://www.gnu.org/licenses/gpl-3.0.txt for full license text.
--- ZKHotkeys.cs
+++ ZKHotkeys.cs
@@ -8,38 +8,37 @@ public class ZKHotkeys : Editor {
 	
 	/* Initialize will build the basic folder structure, and as we progress will load any
 	 * assets that ZoaKo games will use in the future */
-	static bool showAPM = false;
-	static int totalActions = 0;
-	
-	void OnGUI() {
-		if (showAPM) Debug.Log("Showing APM View");
-		GUILayout.Label("Total actions performed this session", EditorStyles.boldLabel);
-		EditorGUILayout.TextField("0", String.Format("{0000}", totalActions));
-		
-	}
+	 static bool showAPM = false;
+	 static int totalActions = 0;
 
-	void Update() {
-		Debug.Log("New action!");
-		if (Event.current.type == EventType.MouseUp) {
-			Debug.Log("New action!");
-			totalActions++;
-		}
-	}
-	
-	[MenuItem("ZKTools/Initialize %i")]
-	static void zk_initialize() {
-		Debug.Log("Initialized ZK Editor Tools!");
-		EditorWindow.GetWindow(typeof(ZKHotkeys));
-	}
-	
-	[MenuItem("ZKTools/Clear Console &x")]
-	static void zk_clear_console() {
-		Debug.Log("Executing Console Clear");
-		//Debug.ClearDeveloperConsole();
+	 void OnGUI() {
+	 	if (showAPM) Debug.Log("Showing APM View");
+	 	GUILayout.Label("Total actions performed this session", EditorStyles.boldLabel);
+	 	EditorGUILayout.TextField("0", String.Format("{0000}", totalActions));
+
+	 }
+
+	 void Update() {
+	 	Debug.Log("New action!");
+	 	if (Event.current.type == EventType.MouseUp) {
+	 		Debug.Log("New action!");
+	 		totalActions++;
+	 	}
+	 }
+
+	 [MenuItem("ZKTools/Initialize %i")]
+	 static void zk_initialize() {
+	 	Debug.Log("Initialized ZK Editor Tools!");
+	 	EditorWindow.GetWindow(typeof(ZKHotkeys));
+	 }
+
+	 [MenuItem("ZKTools/Clear Console &x")]
+	 static void zk_clear_console() {
+	 	Debug.Log("Executing Console Clear [You shouldn't see this message]");
 		Assembly assembly = Assembly.GetAssembly(typeof(SceneView)); 
-	    Type type = assembly.GetType("UnityEditorInternal.LogEntries");
-	    MethodInfo method = type.GetMethod("Clear");
-	    method.Invoke(new object(), null);
+		Type type = assembly.GetType("UnityEditorInternal.LogEntries");
+		MethodInfo method = type.GetMethod("Clear");
+		method.Invoke(new object(), null);
 	}
 	
 	[MenuItem("ZKTools/Create C# script &#3")]
@@ -49,20 +48,20 @@ public class ZKHotkeys : Editor {
 
 	[MenuItem("ZKTools/Create Cube &a")]
 	static void zk_create_cube() {
-		Debug.Log("Create a cube");
+		Debug.Log("Creating cube");
 		EditorApplication.ExecuteMenuItem("GameObject/Create Other/Cube");
 	}
 	
-	[MenuItem("ZKTools/Create Sphere &s")]
+	[MenuItem("ZKTools/Create Quad &s")]
 	static void zk_create_sphere() {
-		Debug.Log("Create a sphere");
-		EditorApplication.ExecuteMenuItem("GameObject/Create Other/Sphere");
+		Debug.Log("Creating quad");
+		EditorApplication.ExecuteMenuItem("GameObject/Create Other/Quad");
 	}
 	
-	[MenuItem("ZKTools/Create Directional Light &d")]
+	[MenuItem("ZKTools/Create Point Light &d")]
 	static void zk_create_d_light() {
-		Debug.Log("Create a directional light");
-		EditorApplication.ExecuteMenuItem("GameObject/Create Other/Directional Light");
+		Debug.Log("Creating Point light");
+		EditorApplication.ExecuteMenuItem("GameObject/Create Other/Point Light");
 	}
 	
 	[MenuItem("ZKTools/Edit Physics &p")]
@@ -71,11 +70,22 @@ public class ZKHotkeys : Editor {
 	}
 
 	[MenuItem ("ZKTools/Activate\\Deactivate selected objects %#d")]
-    static void ChangeActivate() {
-      foreach (GameObject go in Selection.gameObjects) {
-        go.SetActive(!go.activeInHierarchy);
-      }
-    }
+	static void ChangeActivate() {
+		foreach (GameObject go in Selection.gameObjects) {
+			go.SetActive(!go.activeInHierarchy);
+		}
+	}
+
+	[MenuItem ("ZKTools/Activate\\Apply changes to all selected prefabs %&a")]
+	static void MassPrefabApply() {
+		foreach (GameObject go in Selection.gameObjects) {
+			PrefabUtility.ReplacePrefab(go, PrefabUtility.GetPrefabParent(go),
+										ReplacePrefabOptions.ConnectToPrefab);
+		}
+		Debug.Log("Applied to " + Selection.gameObjects.Length + " game object prefabs");
+	}
+
+
 	[MenuItem("ZKTools/Show Editor APM _?")]
 	static void zk_show_APM_view() {		
 		showAPM = !showAPM;
