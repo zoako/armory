5615c7fc6dc342ddbf5f7522f67a81a0528b4922
Keerthik <keerthiko@gmail.com>
2014-06-01 19:36:00 -0700

    Now meant to be installed as a plugin rather than placed in the Editor Folder

---
+++ Editor/Debug/ZKDebug.cs
@@ -0,0 +1,164 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using ZKTools;
+
+public class ZKDebug : EditorWindow {
+
+	// Set a higher debug level to catch more debug statements
+	private static int debugLevel = 10;
+	// Public Usage Calls
+	[MenuItem("ZKTools/ZKDebug Console #%w")]
+	static void CreateOrReattach () {
+		if (window == null) window = (ZKDebug) EditorWindow.GetWindow(typeof(ZKDebug));
+		visible = true;
+
+		Application.RegisterLogCallback(window.HandleLog);
+		window.needsReattachment = false;
+
+		Debug.Log("Reattached at " + System.DateTime.Now.TimeOfDay) ;
+
+		window.attemptedScroll = false;
+		window.scrolledDown = false;
+		window.newLogArrived = false;
+		window.eventsWaited = 0;
+	}
+	
+	public static void Clear() {
+		if (window != null) {
+			window.logs.Clear();
+			window.Repaint();
+		}
+	}
+
+	public static void setDebugLevel(int level) {
+		debugLevel = Mathf.Max (0, level);
+	}
+
+
+	class Log {
+		public string message;
+		public string stackTrace;
+		public LogType type;
+		private GUILayoutOption[] layoutParams;
+
+		public void SetLayout(params GUILayoutOption[] options) {
+			layoutParams = options;
+		}
+
+		public void RenderLog() {
+			EditorGUILayout.SelectableLabel (message + "\n" + stackTrace, 
+			                                 layoutParams);
+		}
+	}
+
+	static bool visible = false;
+	static bool consoleDevMode = true;
+	static float devLabelHeight = 30;
+
+	// Visual elements:
+	static readonly Dictionary<LogType, Color> logTypeColors = new Dictionary<LogType, Color>
+	{
+		{ LogType.Assert, Color.white },
+		{ LogType.Error, Color.red },
+		{ LogType.Exception, Color.red },
+		{ LogType.Log, Color.white },
+		{ LogType.Warning, Color.yellow },
+	};
+
+	private bool IsNearBottom() {
+		return (scrollPosition.y > (scrollBottom - 2f*(logHeight + 10f)));
+	}
+
+	// Layout properties and settings
+	protected static float logHeight = 30;
+	protected static int eventsSinceScrollToWait = 10;
+
+	// Properties we care about
+	readonly List<Log> logs = new List<Log>();
+	bool collapse;
+
+	// Properties used for managing scroll position
+	protected Vector2 scrollPosition;
+
+	// These are pretty much private to the autoscroll system
+	float scrollBottom = 0;
+	bool attemptedScroll = false;
+	bool scrolledDown = false;
+	bool newLogArrived = false;
+	int eventsWaited;
+
+	Vector2 BeginAutoScrollView (params GUILayoutOption[] options) {
+		// Figuring out scroll-mode
+		scrollBottom = logs.Count * (logHeight + 2f) - position.height + devLabelHeight + 10f;
+		bool scrollToBottom = 	!(attemptedScroll && !scrolledDown) && 		// DONT, scrolled up
+								IsNearBottom() && (newLogArrived ||			// DO, looking at last and new log is here
+			                   	(attemptedScroll && scrolledDown));			// DO, scrolling down near the bottom
+		if (scrollToBottom) 
+			return EditorGUILayout.BeginScrollView(new Vector2(0, scrollBottom), options);
+		else 
+			return EditorGUILayout.BeginScrollView(scrollPosition, options);
+	}
+
+	void EndAutoScrollView () {
+		// Manipulating events
+		if (Event.current.type == EventType.scrollWheel) {
+			Debug.Log ("Scrolling");
+			attemptedScroll = true;
+			scrolledDown = Event.current.delta.y > 0;
+			eventsWaited = 0;
+		} else {
+			if (eventsWaited > eventsSinceScrollToWait) attemptedScroll = false;
+			else eventsWaited++;
+		}
+		newLogArrived = false;
+		EditorGUILayout.EndScrollView();
+	}
+	
+	protected void OnGUI () {
+		BeginAutoScrollView (GUILayout.MinHeight(position.height - devLabelHeight - 10f));
+		// Drawing
+		foreach (Log log in logs) {
+			log.RenderLog ();
+		}
+		EndAutoScrollView();
+
+		if (consoleDevMode) {
+			GUILayout.Label ("ZKDebug; atBottom: " + IsNearBottom() + "\nscroll: " + scrollBottom + " position: " + scrollPosition.y + " height: " + position.height, 
+	                 EditorStyles.boldLabel,
+	                 GUILayout.MinHeight(devLabelHeight),
+	                 GUILayout.MaxHeight(devLabelHeight));
+		}	
+	}
+
+	void HandleLog (string message, string stackTrace, LogType type) {
+		logs.Add(new Log {
+			message = message,
+			stackTrace = stackTrace,
+			type = type,
+		});
+		newLogArrived = true;
+		Repaint ();
+	}
+
+	int lastState = 0;
+	private bool needsReattachment = false;
+	void Update() {
+		int thisState = EditorApplication.isCompiling?1:
+						EditorApplication.isUpdating?2:
+						EditorApplication.isPlaying?3:0;
+		needsReattachment = (thisState != lastState);
+		if (needsReattachment) CreateOrReattach();
+		lastState = EditorApplication.isCompiling?1:
+					EditorApplication.isUpdating?2:
+					EditorApplication.isPlaying?3:0;
+	}
+
+	private static ZKDebug window = null;
+	void OnDestroy() {
+		window = null;
+		visible = false;
+		Application.RegisterLogCallback(null);
+	}
+	
+}
---
+++ Utils.cs
@@ -0,0 +1,128 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+namespace ZKTools {
+public class Utils {
+	// String stuff
+	public static string Timerify (float timer) {
+		int displayMinutes = (int)(timer/60);
+		int displaySeconds = (int)(timer)%60;
+		string text = timer<0?"--:--":string.Format ("{0:00}:{1:00}", displayMinutes, displaySeconds);
+		return text;
+	}
+	
+	// Shorthand for unity stuff
+	public static Rect RelRect(float left, float top, float width, float height) {
+		return new Rect(0.01f*left*Screen.width, 0.01f*top*Screen.height, 0.01f*width*Screen.width, 0.01f*height*Screen.height);
+	}
+	
+	public static Vector3 SetX(Vector3 input, float x) {
+		return new Vector3(x, input.y, input.z);
+	}
+	public static Vector3 SetY(Vector3 input, float y) {
+		return new Vector3(input.x, y, input.z);
+	}
+
+	public static Vector3 SetZ( Vector3 input, float z) {
+		return new Vector3(input.x, input.y, z);
+	}
+
+	public static bool SetActive(GameObject obj, bool active) {
+		if (obj.activeInHierarchy == active) return false;
+		obj.SetActive(active);
+		return true;
+	}
+
+	// Math operations
+	public static int mod(int a, int b) {
+		return a - Mathf.FloorToInt((float)a/b)*b;
+	}
+	
+	public static int wrap(int val, int lbd, int ubd) {
+		return lbd + mod(val-lbd, ubd+1-lbd);
+	}
+	
+	/*
+	public static float wrap(float val, float lbd = 0f, float ubd = 1f) {
+		float wrapval = val/(ubd-lbd);
+		return lbd + val - Mathf.Floor(wrapval)*(ubd-lbd);
+	}
+	
+	public static float clamp(float val, float lbd = 0f, float ubd = 1f) {
+		return Mathf.Max (lbd, Mathf.Min(val, ubd));
+	}
+	*/
+
+	public static float Sum(float [] numbers) {
+		float sum = 0;
+		foreach (float f in numbers) sum += f;
+		return sum;
+	}
+	
+	public static int Sum(int [] numbers) {
+		int sum = 0;
+		foreach (int i in numbers) sum += i;
+		return sum;
+	}
+	
+	public static int Sum(bool [] bools) {
+		int sum = 0;
+		foreach (bool b in bools) if (b) sum++;
+		return sum;
+	}
+	
+	public static int indexOfMax(float [] inVals) {
+		int maxIn = -1;
+		for (int i = 0; i < inVals.Length; i++) {
+			if (maxIn < 0 || inVals[i] > inVals[maxIn])
+				maxIn = i;
+		}
+		return maxIn;
+	}
+	
+	public static int indexOfMax(bool [] inVals) {
+		for (int i = 0; i < inVals.Length; i++) {
+			if (inVals[i]) return i;
+		}
+		return -1;
+	}
+	
+	// Mesh tools
+	public static float Area (Mesh m) {
+		Vector3 [] mVertices = m.vertices;
+		
+		float result = 0;
+		for(int p = mVertices.Length-1, q = 0; q < mVertices.Length; p = q++) {
+			result += (Vector3.Cross(mVertices[q], mVertices[p])).magnitude;
+		}
+		
+		result *= 0.5f;
+		return result;		
+	}
+
+	public static Mesh CopyMesh(Mesh orginalMesh) {
+		var newmesh = new Mesh {
+			vertices = orginalMesh.vertices,
+			triangles = orginalMesh.triangles,
+			uv = orginalMesh.uv,
+			normals = orginalMesh.normals,
+			colors = orginalMesh.colors,
+			tangents = orginalMesh.tangents
+		};
+		return newmesh;
+	}
+	
+	// Not really very useful?
+	private static bool NamespaceExists(string namespaceChecked) {
+		var namespaceFound = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+		                      from type in assembly.GetTypes()
+		                      where type.Namespace == namespaceChecked
+		                      select type).Any();
+		
+		return (bool) namespaceFound;
+	}
+	
+}
+}
